fe2688dcf6c8a0e64054794169d35fe3e7230968
Roman <mynameis@romalozhkin.com>
2024-01-11 15:53:04 +0400

    remove haskell tools

--- lua/core/plugins_config/init.lua
+++ lua/core/plugins_config/init.lua
@@ -1,5 +1,4 @@
 require('core.plugins_config.nvim-tree')
 require('core.plugins_config.lualine')
 require('core.plugins_config.sonokai')
-require('core.plugins_config.haskell_tools')
 require('core.plugins_config.lspconfig')
